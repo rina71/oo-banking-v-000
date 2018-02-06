1fde0a406959ef80c5b1be627c12434aa02bc3fa
Yurina Shin <yurinas@gmail.com>
2018-02-06 20:45:54 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,7 +1,7 @@
 class Transfer
   attr_accessor :name, :sender, :receiver, :amount, :status
 
-  def initialize(sender, receiver, amount )
+  def initialize(sender, receiver, amount)
     @sender = sender
     @receiver = receiver
     @status = pending

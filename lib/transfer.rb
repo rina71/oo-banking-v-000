78ab10ad003af16100d1875d7de765477113ffae
Yurina Shin <yurinas@gmail.com>
2018-02-06 21:47:22 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,10 +17,10 @@ class Transfer
   def execute_transaction
     # binding.pry
     if @sender.balance > @amount || @sender.balance = @amount
-      @sender.balance -= @amount 
+      @sender.balance -= @amount
       @receiver.balance += @amount
     self.status = "complete"
-  elsif @sender.balance < @amount
+  elsif @sender.balance < @amount || @sender.valid? == false
     self.status = "rejected"
       return "Transaction rejected. Please check your account balance."
     end
